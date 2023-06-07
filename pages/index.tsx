198cbc93ee7e408f8880813836b7b2d6c3c2d503
Mohammed Raamiz Abbasi <raamizabbasi@gmail.com>
2023-06-06 20:43:19 -0400

    Added RCOS and Github Logo
    I added the RCOS and Github Logo to the footer in the landing page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Feature from "../components/Feature"
 import Header from "../components/Header"
 import Jumbotron from "../components/Jumbotron"
 import features from "../lib/frontend/data/features"
+import Image from "next/image"
 
 const Home: FC = () => {
     return (
@@ -27,8 +28,38 @@ const Home: FC = () => {
             </div>
             <Contact />
             <Row className="home-footer">
-                <Col className="center-container">
+                <Col className="center-container" lg={4}>
+                    <Image
+                        src={require("../public/images/Github_Logo.png")}
+                        alt="Github Logo"
+                        height={40}
+                        width={40}
+                        className="shadow pointer"
+                        onClick={() =>
+                            window.open(
+                                "https://github.com/Raamzeez/IntelliSlides",
+                                "_blank"
+                            )
+                        }
+                    />
+                </Col>
+                <Col className="center-container" lg={4}>
                     <p className="home-footer-text">IntelliSlides 2023</p>
+                </Col>
+                <Col className="center-container" lg={4}>
+                    <Image
+                        src={require("../public/images/RCOS_Logo.png")}
+                        alt="Github Logo"
+                        height={40}
+                        width={40}
+                        className="shadow pointer"
+                        onClick={() =>
+                            window.open(
+                                "https://new.rcos.io/projects/intellislides?semester=202305",
+                                "_blank"
+                            )
+                        }
+                    />
                 </Col>
             </Row>
         </Container>
