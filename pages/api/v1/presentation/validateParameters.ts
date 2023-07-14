7be7af3c5738c18209063a35e6d1339f3a734eaa
Tevetron <friedrich.jonathan4@gmail.com>
2023-07-14 14:38:01 -0400

    fixed back arrow light mode

--- pages/api/v1/presentation/validateParameters.ts
+++ pages/api/v1/presentation/validateParameters.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next/types"
 import errorChecks from "../../../../lib/backend/util/errorChecks"
 import { authenticatedHandler } from "../../../../lib/backend/handlers/auth_guard"
-import { slideLimitHandler } from "../../../../lib/backend/handlers/slide_limit_gaurd"
+import { slideLimitHandler } from "../../../../lib/backend/handlers/slide_limit_guard"
 
 export default authenticatedHandler(
     slideLimitHandler(async (req, res) => {
