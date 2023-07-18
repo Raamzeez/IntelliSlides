5f7c0571e3cd9035db3e6a5327d76cfa0e598403
Mohammed Raamiz Abbasi <raamizabbasi@gmail.com>
2023-07-18 14:35:55 -0400

    Update contact.tsx
    Still working on the Contact page, very rough right now

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,6 @@
 import React from "react"
-import { Container } from "react-bootstrap"
+import Button from "../components/Button"
+import { Container, Form } from "react-bootstrap"
 
 const Contact: React.FC = () => {
     return (
@@ -7,6 +8,51 @@ const Contact: React.FC = () => {
             <h1 className="dynamic-color" style={{ marginTop: 25 }}>
                 Contact
             </h1>
+            <p className="dynamic-color" style={{ marginTop: 25 }}>
+                Email: intellislides.contact@gmail.com
+            </p>
+            <div
+                style={{ height: "60vh", width: "60vw", marginTop: 25 }}
+                className="shadow"
+            >
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label className="dynamic-color">Email</Form.Label>
+                        <Form.Control
+                            type="email"
+                            placeholder="Enter email"
+                            className="dynamic-color"
+                        />
+                    </Form.Group>
+                    <Form.Group
+                        controlId="formBasicEmail"
+                        style={{ marginTop: 20 }}
+                    >
+                        <Form.Label className="dynamic-color">Type</Form.Label>
+                        <Form.Control
+                            type="email"
+                            placeholder="Enter email"
+                            className="dynamic-color"
+                        />
+                    </Form.Group>
+                    <Form.Group
+                        controlId="formBasicEmail"
+                        style={{ marginTop: 20 }}
+                    >
+                        <Form.Label className="dynamic-color">
+                            Message
+                        </Form.Label>
+                        <br />
+                        <textarea name="Text1" cols={40} rows={5}></textarea>
+                    </Form.Group>
+                </Form>
+            </div>
+            <Button
+                style={{ margin: 30 }}
+                type="success"
+                value="Submit"
+                onClickHandler={() => null}
+            />
         </Container>
     )
 }
